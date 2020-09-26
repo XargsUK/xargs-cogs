e559f1329708e20bed19a7a2cbf93f5d2d36c0a5
xargs <hello@xargz.dev>
2020-09-26 17:48:56 +0100

    adding maptest function
    just testing if variables can be pasted for the map command

--- amongushelper/helper.py
+++ amongushelper/helper.py
@@ -8,3 +8,14 @@ class helper(commands.Cog):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("Map!")
+
+    @commands.command()
+    async def maptest(self, ctx, map_name:str):
+        if map_name == "polus"
+            await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/4/4c/Polus.png")
+        elif map_name == "skeld"
+            await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/4/4f/SKELD_MAP.jpg")
+        elif map_name == "mira"
+            await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/0/0a/Mirahq.png")
+        else:
+            await ctx.send("Map not recognised. Please select polus / skeld / mira")
