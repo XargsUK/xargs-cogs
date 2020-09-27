b2f841d1de0490fd54e7ad1184106db685f2c511
xargs <hello@xargz.dev>
2020-09-27 16:57:41 +0100

    fixing indentation

--- amongushelper/helper.py
+++ amongushelper/helper.py
@@ -16,16 +16,14 @@ class amongushelper(commands.Cog):
 
 	__version__ = "1.0.0"
 	__author__ = "xargs"
-
-    @commands.command()
-    async def map(self, ctx, map_name:str=None):
-        if map_name == None
-            await ctx.send("No map specified. Please select polus / skeld / mira")
-        if map_name == "polus"
-            await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/4/4c/Polus.png")
-        elif map_name == "skeld"
-            await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/4/4f/SKELD_MAP.jpg")
-        elif map_name == "mira"
-            await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/0/0a/Mirahq.png")
-        else:
-            await ctx.send("Map name ", map_name, " not recognised.")
+    
+@commands.command()
+async def maptest(self, ctx, map_name:str):
+    if map_name == "polus"
+        await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/4/4c/Polus.png")
+    elif map_name == "skeld"
+        await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/4/4f/SKELD_MAP.jpg")
+    elif map_name == "mira"
+        await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/0/0a/Mirahq.png")
+    else:
+        await ctx.send("Map not recognised. Please select polus / skeld / mira")
