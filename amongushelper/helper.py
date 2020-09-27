288bc57014610cb64af3831107082422dad9ac77
xargs <hello@xargz.dev>
2020-09-27 17:00:22 +0100

    changing string comparison to "is" rather than ==

--- amongushelper/helper.py
+++ amongushelper/helper.py
@@ -19,11 +19,11 @@ class amongushelper(commands.Cog):
     
 @commands.command()
 async def maptest(self, ctx, map_name:str):
-    if map_name == "polus"
+    if map_name is "polus"
         await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/4/4c/Polus.png")
-    elif map_name == "skeld"
+    elif map_name is "skeld"
         await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/4/4f/SKELD_MAP.jpg")
-    elif map_name == "mira"
+    elif map_name is "mira"
         await ctx.send("https://vignette.wikia.nocookie.net/among-us-wiki/images/0/0a/Mirahq.png")
     else:
         await ctx.send("Map not recognised. Please select polus / skeld / mira")
