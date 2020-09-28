32161b81a7c6c72db8e45fb1f6cef4f50f4637d3
xargs <hello@xargz.dev>
2020-09-28 12:28:50 +0100

    assinging mapname variable to none

--- amongushelper/helper.py
+++ amongushelper/helper.py
@@ -22,6 +22,9 @@ class amongushelper(commands.Cog):
         "skeld" : "https://vignette.wikia.nocookie.net/among-us-wiki/images/4/4f/SKELD_MAP.jpg",
         "mira" : "https://vignette.wikia.nocookie.net/among-us-wiki/images/0/0a/Mirahq.png"
     }
+
+    map_name = None
+    
     @commands.command()
     async def map(self, ctx, map_name):
         if self.map_name in self.maps:
